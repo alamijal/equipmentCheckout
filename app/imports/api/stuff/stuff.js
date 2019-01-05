f244248b5ea0b071ff3d814f95975117cc5de542
Jala Alamin <jala.alamin@wsu.edu>
2019-01-04 18:08:19 -0800

    add full info to history page

--- app/imports/api/stuff/stuff.js
+++ app/imports/api/stuff/stuff.js
@@ -3,7 +3,36 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const History = new Mongo.Collection('History');
 export const HistorySchema = new SimpleSchema({
-
+  first2: {
+    label: 'First Name',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: 'John',
+    },
+  },
+  last2: {
+    label: 'Last Name',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: 'Doe',
+    },
+  },
+  number2: {
+    label: 'Phone Number',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: '123-456-7890',
+    },
+  },
   time2: {
     label: 'Check-in Time',
     type: String,
@@ -22,6 +51,32 @@ export const HistorySchema = new SimpleSchema({
     autoform: {
       group: 'History',
       type: 'date',
+    },
+  },
+  equipmentName2: {
+    label: 'Item Name & Number',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: 'HDMI to VGA adapter #2',
+    },
+  },
+  equipmentType2: {
+    label: 'Equipment Type',
+    type: String,
+    optional: false,
+    max: 20,
+    autoform: {
+      group: 'Stuff',
+      placeholder: '123-456-1234',
+      type: 'select-radio-inline',
+      options: function () {
+        return [{ label: 'Laptop', value: 'Laptop' },
+          { label: 'Adapter', value: 'Adapter' }, { label: 'Charger', value: 'Charger' },
+          { label: 'Other', value: 'Other' }];
+      },
     },
   },
 });
