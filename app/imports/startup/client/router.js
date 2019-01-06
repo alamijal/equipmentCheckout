09757ce7c8152386d58847f47724066b835cd4ec
Jala Alamin <jala.alamin@wsu.edu>
2019-01-06 10:36:32 -0800

    added links to buttons

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -108,6 +108,12 @@ FlowRouter.route('/stuff/:_id', {
     BlazeLayout.render('App_Body', { main: 'View__Full_Recipe' });
   },
 });
+FlowRouter.route('/checkin', {
+  name: 'Edit_Stuff_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+  },
+});
 FlowRouter.route('/edit/:_id', {
   name: 'Edit_Page',
   action() {
--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Stuff } from '../../api/stuff/stuff.js';
 
 Template.Home_Page.helpers({
@@ -8,5 +9,9 @@ Template.Home_Page.helpers({
    */
   stuffList() {
     return Stuff.find();
+  },
+  isComment: function isComment() {
+    const curr = Stuff.findOne(FlowRouter.getParam('_id'));
+    return ((curr.comments === '' || curr.comments.length === 0 || curr.comments == null));
   },
 });
